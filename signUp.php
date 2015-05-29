d5fd894c86e9bd218a7f787a00d75c6eeaf916e3
signaleo <diegoourzua@gmail.com>
2015-05-29 01:18:13 -0500

    Test
    this a little test of the library

---
+++ signUp.php
@@ -0,0 +1,9 @@
+
+<form id="signUp"  name="signUp" action="validate.php">
+	<input id="email" type="email" name="email" placeholder="email" value=""> </input>
+	<br>
+	<input type="submit">
+</form>
+<?php
+	
+?>
